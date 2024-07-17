53638d72c2f9e90e404b92006fbda45de008d42b
Dmitry <grafchitaru@yandex.ru>
2024-07-17 13:38:33 +0400

    Add Migrations

---
+++ migrations/20240717124341_create_user_collections.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS "user_collections"
+(
+    user_id uuid NOT NULL REFERENCES users(id),
+    collection_id uuid NOT NULL REFERENCES collections(id),
+    PRIMARY KEY (user_id, collection_id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE user_collections;
+-- +goose StatementEnd
