46445fd46a5ad017a62f02900fe6629caabc4a99
Dmitry <grafchitaru@yandex.ru>
2024-07-21 10:30:56 +0400

    Add contract GetUserCollections

---
+++ internal/handlers/get_user_collections.go
@@ -0,0 +1,24 @@
+package handlers
+
+import (
+	"encoding/json"
+	"github.com/grafchitaru/skillBuilder/internal/middlewares/auth"
+	"net/http"
+)
+
+func (ctx *Handlers) GetUserCollections(res http.ResponseWriter, req *http.Request) {
+	userID, err := auth.GetUserID(req, ctx.Config.SecretKey)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	collections, err := ctx.Repos.GetUserCollections(userID)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	res.WriteHeader(http.StatusOK)
+	json.NewEncoder(res).Encode(collections)
+}
