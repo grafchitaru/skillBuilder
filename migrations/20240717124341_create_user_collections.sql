60064e6148190d7ea47ab565b274c7eeb81318dd
Дмитрий Кириллов <d.kirillov@webinar.ru>
2024-07-30 15:25:59 +0400

    Update migrations

--- migrations/20240717124341_create_user_collections.sql
+++ migrations/20240717124341_create_user_collections.sql
@@ -3,7 +3,7 @@
 CREATE TABLE IF NOT EXISTS "user_collections"
 (
     user_id uuid NOT NULL REFERENCES users(id),
-    collection_id uuid NOT NULL REFERENCES collections(id),
+    collection_id uuid NOT NULL REFERENCES collections(id) ON DELETE CASCADE,
     PRIMARY KEY (user_id, collection_id)
 );
 -- +goose StatementEnd
